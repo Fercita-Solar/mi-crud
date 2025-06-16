18af1a23f16031f4527f28ac71d22727bfa65389
Fercita-Solar <solarflores.fer@gmail.com>
2025-06-16 17:42:10 -0400

    first commit

---
+++ src/componentes/Form.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from "react";
+
+function Form({addOrUpdateItem,itemToEdit}){
+    const[inputValue,setInputValue]=useState('');
+
+    useEffect(()=>{
+        if(itemToEdit){
+            setInputValue(itemToEdit.value);
+        }else{
+            setInputValue('');
+        }
+    },[itemToEdit]);
+    consthandleSubmit = (e) =>{
+        e.preventDefault();
+        if (inputValue.trim()){
+            addOrUpdateItem(inputValue);
+            setInputValue('');
+        }
+    };
+    return(
+        <Form onSubmit={handleSubmit}>
+            <input
+            type="text"
+            value={inputValue}
+            onChange={(e)=>
+    setInputValue(e.target.value)}
+        />
+        <button type="submit">{itemToEdit ?
+        'Actualizar':'Agregar'}</button>
+        </Form>
+    );
+}
+export default Form;
